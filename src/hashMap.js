b81c40731fa2c4c00e6ed2171b0c13c5ad9f464d
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-11-19 19:52:13 -0500

    Finished hashmap methods

--- src/hashMap.js
+++ src/hashMap.js
@@ -56,6 +56,8 @@ export class HashMap {
     this.length++;
 
     if (this.length / this.capacity > this.loadFactor) this.increaseCapacity();
+
+    return `${key}: ${value} was added to the hashmap.`;
   }
 
   get(key) {
@@ -69,5 +71,84 @@ export class HashMap {
     }
 
     return null;
+  }
+
+  has(key) {
+    const hashedKey = this.hash(key);
+    const bucket = this.buckets[hashedKey];
+
+    for (const entry of bucket) {
+      if (entry.key === key) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  remove(key) {
+    const hashedKey = this.hash(key);
+    const bucket = this.buckets[hashedKey];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        bucket.splice(i, 1);
+        this.length--;
+        return `The item at key ${key} was removed from the hash map.`;
+      }
+    }
+
+    return `The item at key ${key} wasn't in the hash map.`;
+  }
+
+  length() {
+    return this.length;
+  }
+
+  clear() {
+    this.capacity = 16;
+    this.length = 0;
+    this.buckets = {};
+    for (let i = 0; i < this.capacity; i++) {
+      this.buckets[i] = [];
+    }
+  }
+
+  keys() {
+    const hashMapKeys = [];
+
+    for (const bucketKey of Object.keys(this.buckets)) {
+      const bucket = this.buckets[bucketKey];
+      for (const entry of bucket) {
+        hashMapKeys.push(entry.key);
+      }
+    }
+
+    return hashMapKeys;
+  }
+
+  values() {
+    const hashMapValues = [];
+
+    for (const bucket of Object.values(this.buckets)) {
+      for (const entry of bucket) {
+        hashMapValues.push(entry.value);
+      }
+    }
+
+    return hashMapValues;
+  }
+
+  entries() {
+    const hashMapKVPs = [];
+
+    for (const bucket of Object.values(this.buckets)) {
+      for (const entry of bucket) {
+        const kvp = [entry.key, entry.value];
+        hashMapKVPs.push(kvp);
+      }
+    }
+
+    return hashMapKVPs;
   }
 }
